83dfa89c9814d492bf42c94f539c03baa4290bcf
Bogdan Protsenko <bogdan.protsenko@MacBook-Air-Bogdan.local>
2021-07-23 22:21:09 +0300

    First commit

---
+++ src/Loader.js
@@ -0,0 +1,7 @@
+import { findByLabelText } from '@testing-library/dom'
+import React from 'react'
+
+export default () => 
+<div style={{display: 'flex', justifyContent: 'center', margin: '.5rem'}}>
+    <div className="lds-ripple"><div></div><div></div></div>
+</div>
